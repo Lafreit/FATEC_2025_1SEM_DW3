edb749ce9e0fede25e82c3c3697c9b5d67931635
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-04-10 21:57:13 -0300

    Mongo + Django

--- django/feriados/core/views.py
+++ django/feriados/core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from datetime import datetime
 from core.models import FeriadoModel
+from core.service import teste
 
 def feriado(requests):
     hoje = datetime.today()
     qs = FeriadoModel.objects.filter(mes=hoje.month)
     qs = qs.filter(dia=hoje.day)
+    teste()
     if len(qs) > 0:
         contexto = {'feriado': True, 'nome':qs[0].nome}
     else:
