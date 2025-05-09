a0da6c2d790cdbb73e45f3c489cd106bb327f018
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-05-08 21:47:50 -0300

    Django Rest Framework - sem autenticação

--- django/feriados/core/urls.py
+++ django/feriados/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import FeriadoDetailView, FeriadoListCreateView
 
 urlpatterns = [
     path('',views.feriado),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('feriados/<int:feriado_id>/deletar/', views.deletar_feriado, name='deletar_feriado'),
     path('feriados/<int:feriado_id>/atualizar/', views.atualizar_feriado, name='atualizar_feriado'),
     path('json_all_feriados', views.listar_feriados_json),
+    path('api/feriados/', FeriadoListCreateView.as_view(), name='api_feriados_list_create'),
+    path('api/feriados/<int:pk>/', FeriadoDetailView.as_view(), name='api_feriados_detail'),
 ]
